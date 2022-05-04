61a0becdf6d42c339c87325b26e1ee970eb48450
David Puentes <dafelpu.farfan95@gmail.com>
2022-05-04 11:02:11 -0500

    fix: delete Link from react-router

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
 import './Header.scss';
 import logo from '../../assets/logo-meli.png';
